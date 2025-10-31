99358366e4237a77df7d53b7d51e3e371456b56e
MLH99 <mohammedluayh@gmail.com>
2025-10-31 13:46:09 +0100

    hidden config

--- src/recipe_automator.py
+++ src/recipe_automator.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
+import os
 import mysql.connector
 
-db = mysql.connector.connect(host="localhost",
-                             user ="root",
-                             passwd = "@sdasdasd@7957@",
-                             database = "recipe_and_ingredients")
+db = mysql.connector.connect(host=os.getenv("DB_HOST"),
+                             user =os.getenv("DB_USER"),
+                             passwd = os.getenv("DB_PASSWORD"),
+                             database = os.getenv("DB_NAME"))
 
 db.autocommit = True
 mycursor = db.cursor(dictionary=True)
