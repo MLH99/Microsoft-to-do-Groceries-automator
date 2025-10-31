ebe75f9505a4203945ca47f28bccc211adb6382c
MLH99 <mohammedluayh@gmail.com>
2025-10-31 19:14:42 +0100

    started with database parser

--- src/database_parser.py
+++ src/database_parser.py
@@ -6,3 +6,27 @@ be a choice. After adding or chaing a recipe you can press OK to apply the chang
 a message which tells you if it succeded or not. You should also have an option to see the current recipes.
 So you can choose which one to view and edit. The editing should not be done one at a time.
 Instead you should be able to click on a table and change its contents."""
+from database_connector import DatabaseConnector
+
+class DatabaseParser():
+
+    def __init__(self):
+        self.db_connector = DatabaseConnector()
+
+    def get_recipe_names(self):
+        # get names from database using recipe_automator
+
+        recipe_names = self.db_connector.get_recipes()
+
+        # return the info as a list
+
+        return recipe_names
+    
+    def get_recipe_ingredients(recipe_name: str):
+        
+        
+
+
+
+
+
